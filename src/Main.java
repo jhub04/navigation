eb5e6c03ee669af32efd833ab3bf8ba2a345a3c1
Your Name <you@example.com>
2024-11-05 17:13:09 +0100

    Added a filereader

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,13 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.LinkedList;
 import java.util.PriorityQueue;
 
 public class Main {
     public static void main(String[] args) {
-        Node A = new Node(1, 1, 1);
-        Node B = new Node(2, 1, 1);
-        Node C = new Node(3, 1, 1);
-        Node D = new Node(4, 1, 1);
-        Node E = new Node(5, 1, 1);
-        Edge AtoB = new Edge(1, B);
-        Edge AtoC = new Edge(4, C);
-        Edge CtoD = new Edge(3, D);
-        Edge BtoC = new Edge(2, C);
-        Edge DtoE = new Edge(5, E);
-        Edge BtoE = new Edge(3, E);
-        A.addEdge(AtoB);
-        A.addEdge(AtoC);
-        B.addEdge(BtoC);
-        C.addEdge(CtoD);
-        D.addEdge(DtoE);
-        B.addEdge(BtoE);
-        ShortestPath sp = new ShortestPath();
-        sp.Dijkstra(A, E);
-        System.out.println(E.distanceToStart);
+        Node []nodes = GraphFileReader.readNodesFromFile("norden/noder.txt");
+        GraphFileReader.readEdgesFromFile("norden/kanter.txt", nodes);
     }
 }
 
@@ -112,4 +97,63 @@ class ShortestPath {
             }
         }
     }
+}
+
+class GraphFileReader{
+    static String[] felt = new String[10];
+
+    public static Node[] readNodesFromFile(String fileName) {
+        System.out.println("Lese nodes");
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+            String line = br.readLine();
+            hsplit(line, 1);
+            int nodeAmount = Integer.parseInt(felt[0]);
+            Node []nodes = new Node[nodeAmount];
+
+            while ((line = br.readLine()) != null) {
+                hsplit(line, 3);
+                int nodeNum = Integer.parseInt(felt[0]);
+                Node node = new Node(nodeNum, Double.parseDouble(felt[1]), Double.parseDouble(felt[2]));
+                nodes[nodeNum] = node;
+            }
+            System.out.println("Ferdig nodes");
+            return nodes;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void readEdgesFromFile(String pathName, Node []nodes) {
+        try (BufferedReader br = new BufferedReader(new FileReader(pathName))) {
+            System.out.println("lese kanter");
+            String line = br.readLine();
+            hsplit(line, 1);
+            int edgeAmount = Integer.parseInt(felt[0]);
+            while ((line = br.readLine()) != null) {
+                hsplit(line, 5);
+                int nodeFromNum = Integer.parseInt(felt[0]);
+                int nodeToNum = Integer.parseInt(felt[1]);
+                int drivingTime = Integer.parseInt(felt[2]);
+                Edge edge = new Edge(drivingTime, nodes[nodeToNum]);
+                nodes[nodeFromNum].addEdge(edge);
+            }
+            System.out.println("Feridg kant");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    static void hsplit(String linje, int antall) {
+        int j = 0;
+        int lengde = linje.length();
+        for (int i = 0; i < antall; ++i) {
+            //Hopp over innledende blanke, finn starten på ordet
+            while (linje.charAt(j) <= ' ') ++j;
+            int ordstart = j;
+            //Finn slutten på ordet, hopp over ikke-blanke
+            while (j < lengde && linje.charAt(j) > ' ') ++j;
+            felt[i] = linje.substring(ordstart, j);
+        }
+    }
+
 }
