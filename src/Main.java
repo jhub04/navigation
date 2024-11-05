f611bcb392e982a069fc1685e478a02111789020
Your Name <you@example.com>
2024-11-05 16:02:21 +0100

    Scuffed dijkstra

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,29 @@
 import java.util.LinkedList;
+import java.util.PriorityQueue;
 
 public class Main {
-    public static void main(String []args) {
-        System.out.println("Hello world");
+    public static void main(String[] args) {
+        Node A = new Node(1, 1, 1);
+        Node B = new Node(2, 1, 1);
+        Node C = new Node(3, 1, 1);
+        Node D = new Node(4, 1, 1);
+        Node E = new Node(5, 1, 1);
+        Edge AtoB = new Edge(1, B);
+        Edge AtoC = new Edge(4, C);
+        Edge CtoD = new Edge(3, D);
+        Edge BtoC = new Edge(2, C);
+        Edge DtoE = new Edge(5, E);
+        A.addEdge(AtoB);
+        A.addEdge(AtoC);
+        B.addEdge(BtoC);
+        C.addEdge(CtoD);
+        D.addEdge(DtoE);
+        ShortestPath sp = new ShortestPath();
+        sp.Dijkstra(A, E);
     }
 }
 
-class Node {
+class Node implements Comparable<Node> {
     int nodeNum;
     double latitude;
     double longitude;
@@ -14,6 +31,7 @@ class Node {
     Node previousNode;
     int distanceToStart;
     int estimatedDistanceToGoal = 0;
+    boolean found = false;
 
     //DEnne for dijkstra
     public Node(int nodeNum, double latitude, double longitude) {
@@ -21,6 +39,7 @@ class Node {
         this.latitude = latitude;
         this.longitude = longitude;
         edges = new LinkedList<>();
+        this.distanceToStart = Integer.MAX_VALUE;
     }
 
     //Bruk denne konstruktør for ALT
@@ -30,6 +49,7 @@ class Node {
         this.longitude = longitude;
         this.estimatedDistanceToGoal = estimatedDistanceToGoal;
         edges = new LinkedList<>();
+        this.distanceToStart = Integer.MAX_VALUE;
     }
 
     public void addEdge(Edge edge) {
@@ -44,10 +64,49 @@ class Node {
         this.distanceToStart = distanceToStart;
     }
 
+    @Override
+    public int compareTo(Node o) {
+        if (this.distanceToStart + this.estimatedDistanceToGoal < o.distanceToStart + o.estimatedDistanceToGoal) {
+            return -1;
+        } else if (this.distanceToStart + this.estimatedDistanceToGoal > o.distanceToStart + o.estimatedDistanceToGoal) {
+            return 1;
+        } else return 0;
+    }
 }
 
 class Edge {
     int drivingTime; //I hundredels sekunder
-    Node from;
     Node to;
+
+    public Edge(int drivingTime, Node to) {
+        this.drivingTime = drivingTime;
+        this.to = to;
+    }
+}
+
+class ShortestPath {
+    public void Dijkstra(Node startNode, Node goalNode) {
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        startNode.distanceToStart = 0;
+        pq.add(startNode);
+        pq.add(goalNode);
+        while (!goalNode.found) {
+            Node exploreNode = pq.poll();
+            exploreNode.found = true;
+            for (Edge edge : exploreNode.edges) {
+                if(!edge.to.found) {
+                    pq.add(edge.to);
+                    System.out.println("Reach");
+                    if (edge.to.previousNode == null) {
+                        edge.to.previousNode = exploreNode;
+                        edge.to.distanceToStart = edge.to.distanceToStart + edge.drivingTime;
+                    } else if (edge.to.distanceToStart > exploreNode.distanceToStart + edge.drivingTime) {
+                        //TODO legge til estimatedGoalDistance i else if greia
+                        edge.to.previousNode = exploreNode;
+                        edge.to.distanceToStart = edge.to.distanceToStart + edge.drivingTime;
+                    }
+                }
+            }
+        }
+    }
 }
